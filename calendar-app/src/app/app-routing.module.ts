98d2c4117ef15bb34dedf6ad78a4ef4fe98d9161
Ajay Vairavan <ajayvairam.krishna@gmail.com>
2019-11-02 12:00:02 -0500

    13 Authentication started

---
+++ calendar-app/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardBodyComponent } from './dashboard-body/dashboard-body.component';
+import { AuthComponent } from './auth/auth.component';
+import { CalendarEditComponent } from './calendar-edit/calendar-edit.component';
+
+const routes: Routes = [
+    { path: 'dashboard', component: DashboardBodyComponent },
+    { path: 'auth', component: AuthComponent },
+    { path: 'editCalendar', component: CalendarEditComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
