52a176eb222edeeee6af3928435ab6e625011a12
Ajay Vairavan <ajayvairam.krishna@gmail.com>
2019-11-19 20:05:00 -0600

    21 Pull Data Error

--- calendar-app/src/app/dashboard-body/card-calendar/card-calendar.component.ts
+++ calendar-app/src/app/dashboard-body/card-calendar/card-calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,6 +9,8 @@ import { Router } from '@angular/router';
 export class CardCalendarComponent implements OnInit {
 
     constructor(private router: Router) { }
+
+    @Input() title: string = 'Honors Accounting';
 
     onAddCalendarClick() {
         console.log("clijsdf");
--- calendar-app/src/app/dashboard-body/dashboard-body.component.ts
+++ calendar-app/src/app/dashboard-body/dashboard-body.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../auth/auth.service';
+import { map, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'dashboard-body',
@@ -6,12 +10,40 @@ import { Component, OnInit, ElementRef } from '@angular/core';
   styleUrls: ['./dashboard-body.component.css']
 })
 export class DashboardBodyComponent implements OnInit {
-  ngOnInit() {
-  }
-  constructor(private elementRef: ElementRef) {
+
+
+
+    calendars: any = [];
+
+    ngOnInit() {
+        this.getCalendar();
+    }
+    constructor(private elementRef: ElementRef, private http: HttpClient, private authService: AuthService) {
 
   }
   ngAfterViewInit() {
     this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#E8F1F2';
-  }
+    }
+
+    onAddNewCourse(title: string = 'AP Chemistry', lastName: string = 'Schulte') {
+        this.http.put('https://app-calendar-65dc1.firebaseio.com/userInformation/' + this.authService.currentUser.id + '/calendars/' + title + '/.json?auth=' + this.authService.currentUser.token,
+            {
+                name: title,
+                owner: lastName
+            }
+        ).subscribe(responseData => {
+            this.calendars = this.getCalendar();
+        });
+    }
+
+    getCalendar() {
+        return this.http.get<calendarFormat[]>('https://app-calendar-65dc1.firebaseio.com/userInformation/' + this.authService.currentUser.id + '/calendars/.json?auth=' + this.authService.currentUser.token).pipe(map((data: any) => data.result),
+            catchError(error => { return throwError('Its a Trap!') })
+        );
+    };
+}
+
+interface calendarFormat {
+    title: string,
+    lastName: string
 }
