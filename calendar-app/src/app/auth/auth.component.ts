e7822ce6c3afa2279879a1a559aaa2046723d834
Ajay Vairavan <ajayvairam.krishna@gmail.com>
2019-12-22 23:18:00 -0600

    36 Login Form Validation Started

--- calendar-app/src/app/auth/auth.component.ts
+++ calendar-app/src/app/auth/auth.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { AuthService } from './auth.service';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { HttpClient, HttpParams } from '@angular/common/http';
@@ -18,27 +18,36 @@ export class AuthComponent implements OnInit {
     error: string = null;
 
     onSwitchMode() {
-        this.isLoginMode = !this.isLoginMode;
+        this.router.navigate(['/auth/' + !this.isLoginMode]);
+        this.error = null;
     }
 
     authenticationForm: FormGroup;
     accountCreationForm: FormGroup;
     professions = ['Student', 'Teacher'];
 
-    constructor(private db: AngularFireDatabase, private fb: FormBuilder, private authService: AuthService, private router: Router, private elementRef: ElementRef, private http: HttpClient, private route: ActivatedRoute) { }
-
-    ngOnInit() {
+    constructor(private db: AngularFireDatabase, private fb: FormBuilder, private authService: AuthService, private router: Router, private elementRef: ElementRef, private http: HttpClient, private route: ActivatedRoute)
+    {
         this.authenticationForm = this.fb.group({
-            'emailAuthentication': this.fb.control(null),
-            'passwordAuthentication': this.fb.control(null)
+            'emailAuthentication': this.fb.control(null, [Validators.required, Validators.email]),
+            'passwordAuthentication': this.fb.control(null, [Validators.required])
         })
 
         this.accountCreationForm = this.fb.group({
-            'emailCreation': this.fb.control(null),
-            'passwordCreation': this.fb.control(null),
-            'confirmPasswordCreation': this.fb.control(null),
-            'profession': this.fb.control(null)
+            'emailCreation': this.fb.control(null, [Validators.required, Validators.email]),
+            'profession': this.fb.control(null, [Validators.required]),
+            'password': this.fb.group({
+            'passwordCreation': this.fb.control(null, [Validators.required]),
+                'confirmPasswordCreation': this.fb.control(null, [Validators.required])
+            }, { validators: this.confirmPassword })
+
         })
+
+
+    }
+
+    ngOnInit() {
+
 
         this.route.params.subscribe(
             (params: Params) => {
@@ -55,69 +64,84 @@ export class AuthComponent implements OnInit {
     }
 
     onAccountCreation(signUpData) {
-        if (!this.accountCreationForm.valid) {
-            return;
+        if (this.accountCreationForm.valid) {
+            this.isLoading = true;
+            const email = signUpData.emailCreation;
+            const password = signUpData.passwordCreation;
+            this.authService.signup(email, password).subscribe(
+                resData => {
+                    this.authService.login(email, password).subscribe(
+                        resData => {
+                            this.isLoading = false;
+                            this.http.put<userInformation>('https://app-calendar-65dc1.firebaseio.com/userInformation/' + this.authService.currentUser.id + '/.json?auth=' + this.authService.currentUser.token,
+                                new userInformation(this.authService.currentUser.id, signUpData.profession)
+                            ).subscribe(responseData => {
+                                this.authService.getProfession().subscribe(resData => {
+                                    if (resData == 'Student') {
+                                        this.isLoading = false;
+                                        this.router.navigate(['/studentDashboard']);
+                                    } else if (resData == 'Teacher') {
+                                        this.isLoading = false;
+                                        this.router.navigate(['/dashboard']);
+                                    }
+                                });
+                            });
+                        })
+                },
+                error => {
+                    console.log(error);
+                    this.error = 'An error occured';
+                    this.isLoading = false;
+                });
+            this.accountCreationForm.reset();
+        } else {
+            this.error = 'Please enter Valid Data';
+            console.log(this.accountCreationForm)
         }
 
-        this.isLoading = true;
-        const email = signUpData.emailCreation;
-        const password = signUpData.passwordCreation;
-        this.authService.signup(email, password).subscribe(
-            resData => {
-                this.authService.login(email, password).subscribe(
-                    resData => {
-                        this.isLoading = false;
-                        this.http.put<userInformation>('https://app-calendar-65dc1.firebaseio.com/userInformation/' + this.authService.currentUser.id + '/.json?auth=' + this.authService.currentUser.token, 
-                            new userInformation(this.authService.currentUser.id, signUpData.profession)
-                        ).subscribe(responseData => {
-                            this.authService.getProfession().subscribe(resData => {
-                                if (resData == 'Student') {
-                                    this.isLoading = false;
-                                    this.router.navigate(['/studentDashboard']);
-                                } else if (resData == 'Teacher') {
-                                    this.isLoading = false;
-                                    this.router.navigate(['/dashboard']);
-                                }
-                            });
-                        });
-                    })
-        },
-        error => {
-            console.log(error);
-            this.error = 'An error occured';
-            this.isLoading = false;
-        });
-        this.accountCreationForm.reset();
+
     }
 
     onAuthentication(authenticationInfo) {
-        if (!this.authenticationForm.valid) {
-            return;
+        if (this.authenticationForm.valid) {
+            this.isLoading = true;
+            const email = authenticationInfo.emailAuthentication;
+            const password = authenticationInfo.passwordAuthentication;
+            this.authService.login(email, password).subscribe(
+                resData => {
+                    this.authService.getProfession().subscribe(resData => {
+                        if (resData == 'Student') {
+                            this.isLoading = false;
+                            this.router.navigate(['/studentDashboard']);
+                        } else if (resData == 'Teacher') {
+                            this.isLoading = false;
+                            this.router.navigate(['/dashboard']);
+                        }
+                    });
+                },
+                error => {
+                    console.log(error);
+                    this.error = 'An error occured';
+                    this.isLoading = false;
+                });
+        } else {
+            this.error = 'Please enter Valid Data';
+            console.log(this.accountCreationForm)
         }
 
-        this.isLoading = true;
-        const email = authenticationInfo.emailAuthentication;
-        const password = authenticationInfo.passwordAuthentication;
-        this.authService.login(email, password).subscribe(
-            resData => {
-                this.authService.getProfession().subscribe(resData => {
-                    if (resData == 'Student') {
-                        this.isLoading = false;
-                        this.router.navigate(['/studentDashboard']);
-                    } else if (resData == 'Teacher') {
-                        this.isLoading = false;
-                        this.router.navigate(['/dashboard']);
-                    }
-                });
-            },
-            error => {
-                console.log(error);
-                this.error = 'An error occured';
-                this.isLoading = false;
-            });
+
     }
 
     ngAfterViewInit() {
         this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = 'aliceblue';
+    }
+
+    confirmPassword(group: FormGroup): { [s: string]: boolean } {
+        if (!(group.get('passwordCreation').value == group.get('confirmPasswordCreation').value)) {
+            group.get('confirmPasswordCreation').setErrors({ 'not equivalent': true });
+        } else {
+            group.get('confirmPasswordCreation').setErrors(null);
+        }
+        return;
     }
 }
